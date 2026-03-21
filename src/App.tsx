e0eddb68e13092ee42f4d57dd69faf40ca37bfc8
Prem <karanprem01@gmail.com>
2026-03-21 12:23:40 +0530

    Added page transition animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Portfolio from "./pages/Portfolio"
-import Services from "./pages/Services"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 
-const App = () => {
+import PageTransition from "./components/PageTransition";
+import ScrollToTop from "./components/ScrollToTop";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element= {<Home/>}/>
-      <Route path="/portfolio" element= {<Portfolio/>}/>
-      <Route path="/services" element= {<Services/>}/>
-      <Route path="/about" element= {<About/>}/>
-      <Route path="/contact" element= {<Contact/>}/>
-    </Routes>
-  )
+    <>
+      <ScrollToTop />
+
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+
+          <Route path="/" element={
+            <PageTransition><Home /></PageTransition>
+          } />
+
+          <Route path="/about" element={
+            <PageTransition><About /></PageTransition>
+          } />
+
+          <Route path="/services" element={
+            <PageTransition><Services /></PageTransition>
+          } />
+
+          <Route path="/contact" element={
+            <PageTransition><Contact /></PageTransition>
+          } />
+
+          <Route path="/portfolio" element={
+            <PageTransition><Portfolio /></PageTransition>
+          } />
+
+        </Routes>
+      </AnimatePresence>
+    </>
+  );
 }
 
-export default App
+export default AnimatedRoutes;
